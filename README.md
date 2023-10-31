d23fc7ee0e21c44cc846f350a6dc41bf6097245f
Peter Sullivan <pvsone@gmail.com>
2023-10-31 10:00:33 -0500

    initial import

---
+++ README.md
@@ -0,0 +1,20 @@
+# Temporal GRPC Proxy
+
+## Set up
+Set the following environment variables, e.g:
+```sh
+export TEMPORAL_ADDRESS=my-namespace.my-account.tmprl.cloud:7233
+export TEMPORAL_TLS_CERT=/path/to/cert.crt
+export TEMPORAL_TLS_KEY=/path/to/cert.key
+```
+
+## Run the proxy
+```sh
+go run main.go
+```
+The server will listen on `127.0.0.1:7233`
+
+## Run a sample through the proxy
+Go to your favorite Temporal sample from any of the samples repos.  In the `worker` and `starter` main functions, update the `client` parameters to set your namespace, e.g. `my-namespace.my-account`.
+
+After the above change, run the sample as documented.  It will now run against Temporal Cloud, through the proxy!
---
+++ main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"crypto/tls"
+	"flag"
+	"log"
+	"net"
+	"os"
+
+	"go.temporal.io/api/workflowservice/v1"
+	"go.temporal.io/sdk/client"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+// command line flags
+var bindAddress string
+var upstreamAddress string
+var clientCert string
+var clientKey string
+
+func init() {
+	flag.StringVar(&bindAddress, "bindAddress", "localhost:7233", "Address to bind to")
+	flag.StringVar(&upstreamAddress, "upstreamAddress", os.Getenv("TEMPORAL_ADDRESS"), "host:port for upstream Temporal frontend service [$TEMPORAL_ADDRESS]")
+	flag.StringVar(&clientCert, "tls_cert_path", os.Getenv("TEMPORAL_TLS_CERT"), "Path to client x509 certificate [$TEMPORAL_TLS_CERT]")
+	flag.StringVar(&clientKey, "tls_key_path", os.Getenv("TEMPORAL_TLS_KEY"), "Path to client certificate private key [$TEMPORAL_TLS_KEY]")
+}
+
+func main() {
+	flag.Parse()
+
+	// parse flags, load certs and create transport credentials for grpc client
+	transportCredentials := insecure.NewCredentials()
+	if clientCert != "" && clientKey != "" {
+		cert, err := tls.LoadX509KeyPair(clientCert, clientKey)
+		if err != nil {
+			log.Fatalf("failed to load client cert and key: %v", err)
+		}
+
+		tlsConfig := &tls.Config{
+			Certificates: []tls.Certificate{cert},
+		}
+
+		transportCredentials = credentials.NewTLS(tlsConfig)
+	}
+
+	// create grpc client
+	grpcClient, err := grpc.Dial(
+		upstreamAddress,
+		grpc.WithTransportCredentials(transportCredentials),
+	)
+	if err != nil {
+		log.Fatalf("failed to create grpc client: %v", err)
+	}
+	defer grpcClient.Close()
+
+	// create workflow client from grpc client
+	workflowClient := workflowservice.NewWorkflowServiceClient(grpcClient)
+	if err != nil {
+		log.Fatalf("failed to create workflow client: %v", err)
+	}
+
+	// create tcp listener on bind port
+	listener, err := net.Listen("tcp", bindAddress)
+	if err != nil {
+		log.Fatalf("failed to create tcp listener: %v", err)
+	}
+
+	// create grpc server and register workflow service proxy handler
+	server := grpc.NewServer()
+	handler, err := client.NewWorkflowServiceProxyServer(
+		client.WorkflowServiceProxyOptions{Client: workflowClient},
+	)
+	if err != nil {
+		log.Fatalf("failed to create service proxy: %v", err)
+	}
+	workflowservice.RegisterWorkflowServiceServer(server, handler)
+
+	// start grpc server
+	log.Println("proxy server listening on", listener.Addr().String())
+	err = server.Serve(listener)
+	if err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
